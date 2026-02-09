c0ef374301b5432cd96eededec9b2072aaff5fe5
ThePolishCat <damianadam000@gmail.com>
2026-02-09 12:59:43 +0100

    fx 6

--- system/modules.nix
+++ system/modules.nix
@@ -1,1 +1,12 @@
 { vars, ... }:
+{
+  imports = [
+    ./modules/boot.nix
+    ./modules/networking.nix
+    ./modules/nixos.nix
+    ./modules/programs.nix
+    ./modules/time.nix
+    ./modules/users.nix
+
+  ];
+}
