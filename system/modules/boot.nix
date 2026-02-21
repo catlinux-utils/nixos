0304b8cb66bc872db8e261909ee645aa84972984
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-02-21 23:08:47 +0100

    move to limine, add memtest, add secure boot

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -1,17 +1,48 @@
 { pkgs, vars, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 6;
-  boot.loader.efi = {
-    canTouchEfiVariables = true;
-    efiSysMountPoint = "/efi";
+
+  boot = {
+
+    kernelPackages = pkgs.linuxPackages_zen;
+    initrd.systemd.enable = true;
+
+    loader = {
+
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/efi";
+      };
+
+      limine = {
+        enable = true;
+        enableEditor = false;
+        panicOnChecksumMismatch = true;
+        secureBoot.enable = true;
+        resolution = "max";
+        maxGenerations = 4;
+        style.wallpapers = [ ];
+        extraEntries = ''
+          /Windows
+            protocol: efi_chainload
+            path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+            comment: Microsoft bootloader
+          /Memtest86
+            protocol: linux
+            kernel_path: boot():/limine/efi/memtest86/memtest.efi
+            comment: Memtest86+
+        '';
+        additionalFiles = {
+          "efi/memtest86/memtest.efi" = "${pkgs.memtest86plus.efi}";
+        };
+
+      };
+    };
   };
-  boot.initrd = {
-    systemd.enable = true;
-  };
-  environment.systemPackages = [
-    pkgs.efibootmgr
+  environment.systemPackages = with pkgs; [
+    efibootmgr
+    sbctl
+    memtest86plus
+
   ];
 }
