175830355f0709306d8ee44d5ad38d7bec189d0a
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-03-15 19:18:31 +0100

    ollama fixes

--- system/modules/ollama.nix
+++ system/modules/ollama.nix
@@ -8,13 +8,28 @@ with lib;
 
 {
   config = mkIf (vars.modules.ollama.enable or false) {
+
+    environment.systemPackages = with pkgs; [
+      nodejs_22
+      cmake
+      glibc
+      glibc.dev
+      gnumake
+      ninja
+      gcc
+    ];
+
     services.ollama = {
       enable = true;
       package = pkgs.ollama-rocm;
       rocmOverrideGfx = "10.3.0";
+      environmentVariables = {
+        OLLAMA_FLASH_ATTENTION = "0";
+        #HSA_OVERRIDE_GFX_VERSION = "10.3.0"; # helps ROCm recognize gfx1030
+      };
+
     };
     boot.initrd.kernelModules = [ "amdgpu" ];
-
   };
 
 }
