499645e59d9e31f1441b050f62b18b31932f8797
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-03-15 19:18:24 +0100

    add nh

---
+++ home/modules/shell-apps/hn.nix
@@ -0,0 +1,11 @@
+{ vars, ... }:
+
+{
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.dates = "weekly";
+    clean.extraArgs = "--keep 5 --keep-since 3d";
+    flake = vars.flakeLocation;
+  };
+}
