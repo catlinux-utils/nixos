6cffd952b7710a8eb1467ab9b2def7483b19fe75
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-03-15 20:31:55 +0100

    locale tests

--- system/modules/nixos.nix
+++ system/modules/nixos.nix
@@ -7,9 +7,18 @@
     "C.UTF-8/UTF-8"
     "en_US.UTF-8/UTF-8"
   ];
+  i18n.supportedLocales = [
+    "C.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "pl_PL.UTF-8/UTF-8"
+  ];
 
   i18n.extraLocaleSettings = {
     LC_ALL = "${vars.defaultLocale}";
+  };
+  services.xserver.xkb = {
+    layout = "pl,us";
+    variant = "";
   };
 
   #security.polkit.enable = true;
