57612ba25542503c95f780b7fe685da2c7036fdf
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-03-22 15:05:57 +0100

    add rimsort latest version

--- system/modules/nixos.nix
+++ system/modules/nixos.nix
@@ -38,6 +38,21 @@
     };
     overlays = [
       inputs.nix-vscode-extensions.overlays.default
+      (final: prev: {
+        rimsort = prev.rimsort.overrideAttrs (oldAttrs: rec {
+          version = "1.0.76";
+
+          # nix-prefetch-git --fetch-submodules "https://github.com/RimSort/RimSort" --rev v1.0.76
+
+          src = prev.fetchFromGitHub {
+            owner = "RimSort";
+            repo = "RimSort";
+            rev = "v${version}";
+            hash = "sha256-EO1j4GPRQSB+QEF4tB87x4nCUKpdWU9aGlDFghwxar0=";
+            fetchSubmodules = true;
+          };
+        });
+      })
     ];
   };
 
