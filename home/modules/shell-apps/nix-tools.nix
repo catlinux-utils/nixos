84155196cb98709df12e75989fac90c9a5db237e
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-03-28 15:03:33 +0100

    nh up

--- home/modules/shell-apps/nix-tools.nix
+++ home/modules/shell-apps/nix-tools.nix
@@ -10,8 +10,8 @@ with lib;
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.dates = "weekly";
-    clean.extraArgs = "--keep 5 --keep-since 3d";
+    clean.dates = "daily";
+    clean.extraArgs = "--keep 4 --keep-since 3d";
     flake = vars.flakeLocation + "#" + vars.conf-name;
   };
   home.packages = with pkgs; [
