a299940928758a7a9bc90d4a0398be1cac9d0043
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-04-20 16:00:09 +0200

    uwsm hyprland gdm fix?

--- system/modules/programs/hyprland.nix
+++ system/modules/programs/hyprland.nix
@@ -6,18 +6,18 @@ with lib;
   config = mkIf (vars.modules.desktop-environment.hyprland.enable or false) {
 
     # FIXME: https://github.com/NixOS/nixpkgs/issues/484328
-    services.displayManager.defaultSession = "hyprland-uwsm";
+    # services.displayManager.defaultSession = "hyprland-uwsm";
 
-    programs.uwsm = {
-      enable = true;
-      waylandCompositors = {
-        hyprland = {
-          prettyName = "Hyprland";
-          comment = "Hyprland compositor managed by UWSM";
-          binPath = "/run/current-system/sw/bin/start-hyprland";
-        };
-      };
-    };
+    # programs.uwsm = {
+    #   enable = true;
+    #   waylandCompositors = {
+    #     hyprland = {
+    #       prettyName = "Hyprland";
+    #       comment = "Hyprland compositor managed by UWSM";
+    #       binPath = "/run/current-system/sw/bin/start-hyprland";
+    #     };
+    #   };
+    # };
     # FIXME END
     programs.hyprland = {
       enable = true;
