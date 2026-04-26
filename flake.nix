d732a540161d676a0eb3b1135e7bde011bf3f79c
ThePolishCat <88453875+PolskiKocurek@users.noreply.github.com>
2026-04-27 00:37:16 +0200

    cleanup

--- flake.nix
+++ flake.nix
@@ -8,11 +8,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    qtengine = {
-      url = "github:kossLAN/qtengine";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
