3fbaf46377f74d6d159a5b5a9b00c1de3fd122d4
ThePolishcat <a@a.a>
2026-08-24 20:38:14 +0200

    hosts: add missing laptop-main configuration

--- hosts/laptop-main/config-modules.nix
+++ hosts/laptop-main/config-modules.nix
@@ -1,1 +1,26 @@
-{ ... }: { }
+rec {
+  conf-name = "laptop-main";
+  user = "cat";
+  initialPassword = "cat";
+  networkingHostName = "nixos-laptop-main";
+  timezone = "Europe/Warsaw";
+  defaultLocale = "pl_PL.UTF-8";
+  flakeLocation = "/home/${user}/github/nixos";
+  modules = {
+    display-manager = {
+      greeter.enable = true;
+    };
+    desktop-environment = {
+      hyprland.enable = true;
+    };
+    home-manager = {
+      packages = {
+        git = {
+          name = "ThePolishCat";
+          email = "88453875+PolskiKocurek@users.noreply.github.com";
+
+        };
+      };
+    };
+  };
+}
