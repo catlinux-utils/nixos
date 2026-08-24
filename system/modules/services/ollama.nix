969769c638d16a33b65ada6e49919819753c526f
ThePolishcat <a@a.a>
2026-08-24 20:28:28 +0200

    ollama: drop unneeded build toolchain and duplicate HSA override

--- system/modules/services/ollama.nix
+++ system/modules/services/ollama.nix
@@ -9,27 +9,14 @@ with lib;
 {
   config = mkIf (vars.modules.ollama.enable or false) {
 
-    environment.systemPackages = with pkgs; [
-      nodejs_22
-      cmake
-      glibc
-      glibc.dev
-      gnumake
-      ninja
-      gcc
-    ];
-
     services.ollama = {
       enable = true;
       package = pkgs.ollama-rocm;
-      rocmOverrideGfx = "10.3.0";
+      rocmOverrideGfx = "10.3.0"; # sets HSA_OVERRIDE_GFX_VERSION internally
       environmentVariables = {
         OLLAMA_FLASH_ATTENTION = "0";
-        HSA_OVERRIDE_GFX_VERSION = "10.3.0"; # helps ROCm recognize gfx1030
         HIP_VISIBLE_DEVICES = "0";
-
       };
-
     };
     boot.initrd.kernelModules = [ "amdgpu" ];
   };
